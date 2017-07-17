d05c8f62e4c5146d0e6a97653f84a92144b10b9a
Suman Mukherjee <sumanmukherjee03@gmail.com>
2017-07-17 08:06:15 -0700

    Updated the deps file and the autoenv file

--- bin/install_linux_deps.sh
+++ bin/install_linux_deps.sh
@@ -1,8 +1,10 @@
 #! /usr/bin/env bash
 
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
-echo 'export PATH="/home/ubuntu/.pyenv/bin:$PATH"' >> ~/.bashrc
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+pip install autoenv
+echo "source `which activate.sh`" >> ~/.bashrc
 source ~/.bashrc
 pip install -r requirements.txt
