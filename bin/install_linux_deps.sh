a4886530a6094ae6a1ca67c9c0ddd2f9f48c0060
Suman Mukherjee <sumanmukherjee03@gmail.com>
2017-07-17 15:45:07 -0700

    Trigger the cd hook for pyenv and install the requirements

--- bin/install_linux_deps.sh
+++ bin/install_linux_deps.sh
@@ -1,10 +1,13 @@
 #! /usr/bin/env bash
 
+THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+ROOT_DIR="$THIS_DIR/.."
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
 echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
 pip install autoenv
 echo "source `which activate.sh`" >> ~/.bashrc
 source ~/.bashrc
+pushd .
+cd "$ROOT_DIR"
 pip install -r requirements.txt
+popd
