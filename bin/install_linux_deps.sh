412e41b8261baa6f5eaeb2842dabf5d26dbfe85c
Suman Mukherjee <sumanmukherjee03@gmail.com>
2017-07-25 07:53:07 -0700

    Upgrade pip while installing the deps

--- bin/install_linux_deps.sh
+++ bin/install_linux_deps.sh
@@ -2,6 +2,7 @@
 
 THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$THIS_DIR/.."
+pip install --upgrade pip
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
 echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
 pip install autoenv
