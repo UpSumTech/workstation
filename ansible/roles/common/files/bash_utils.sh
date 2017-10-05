e992b1d12f96eac1af2ccabde1d152d6bdcb06c0
Suman Mukherjee <sumanmukherjee03@gmail.com>
2017-10-04 19:04:40 -0700

    Updated the bash utils

--- ansible/roles/common/files/bash_utils.sh
+++ ansible/roles/common/files/bash_utils.sh
@@ -1,3 +1,12 @@
+function install_go_deps_for_project() {
+  pushd .
+  while [[ ! $(find . -maxdepth 1 -type d | grep '.git') =~ './.git' && ! $(basename $(cd $PWD/../.. && pwd)) =~ (github.com|golang.org|google.golang.org|gopkg.in) ]]; do
+    cd ..
+  done
+  go get -u $(find . -maxdepth 1 ! -path . ! -path '*/\.*' -type d | grep -v vendor | xargs -n 1 -I % echo %/...)
+  popd
+}
+
 function start_docker_machine() {
   (docker-machine ls | awk '{if(NR>1) print $1,$3,$4,$5}' | grep -i '^default virtualbox' 2>&1 >/dev/null \
     || docker-machine create --driver virtualbox default) &
@@ -5,6 +14,24 @@ function start_docker_machine() {
   (env | grep DOCKER | grep DOCKER_HOST | cut -d '=' -f2 | sed -e 's#tcp://##g;s#:# #g' | xargs nc -v \
     || (docker-machine stop default 2>&1 >/dev/null; eval "$(docker-machine env -u)"; docker-machine start default; eval "$(docker-machine env default)")) &
   wait
+}
+
+function stop_docker_machine() {
+  (docker-machine status default | grep -i "running" && (docker-machine stop default)) &
+  wait
+  eval "$(docker-machine env -u)"
+}
+
+function start_minikube() {
+  (minikube status | grep -i "running" || (minikube start)) &
+  wait
+  eval "$(minikube docker-env)"
+}
+
+function stop_minikube() {
+  (minikube status | grep -i "running" && (minikube stop)) &
+  wait
+  unset ${!DOCKER_*}
 }
 
 function switch_to_minikube() {
