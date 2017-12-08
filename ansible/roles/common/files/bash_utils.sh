53bd588114058aa61484eb8131b15db6f2188360
Suman Mukherjee <sumanmukherjee03@gmail.com>
2017-12-08 11:46:18 -0800

    Updated the bash util to handle an increased size for the default docker machine

--- ansible/roles/common/files/bash_utils.sh
+++ ansible/roles/common/files/bash_utils.sh
@@ -14,7 +14,7 @@ function install_go_deps_for_project() {
 
 function start_docker_machine() {
   (docker-machine ls | awk '{if(NR>1) print $1,$3,$4,$5}' | grep -i '^default virtualbox' 2>&1 >/dev/null \
-    || docker-machine create --driver virtualbox default) &
+    || docker-machine create --driver virtualbox --virtualbox-memory "2048" --virtualbox-disk-size "40000" default) &
   eval "$(docker-machine env default)"
   (env | grep DOCKER | grep DOCKER_HOST | cut -d '=' -f2 | sed -e 's#tcp://##g;s#:# #g' | xargs nc -v \
     || (docker-machine stop default 2>&1 >/dev/null; eval "$(docker-machine env -u)"; docker-machine start default; eval "$(docker-machine env default)")) &
